d7354a5f4649018833b50e634c38718c7e38ef7c
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-18 12:29:12 +0000

    [Continue]bookers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,13 @@ class UsersController < ApplicationController
   def index
   end
   
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. トップ画面へリダイレクト
+    redirect_to root_path
+  end
+  
 end
