ad05d6f4e4aab6e0987ce530c532bfb2bffc310e
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-22 05:31:44 +0000

    [Finish]bookers22

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top', as: 'root'
-  get 'home/about' => 'home#index'
+  get 'home/about' => 'homes#index'
+  #home/aboutのURLに飛んだ時、homesコントローラのindexアクションを実行
   resources :books, only: [:new, :create, :index, :edit, :show, :destroy]
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
