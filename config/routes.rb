e8495e00c911dba30e8949fe8dd05138fbd0995a
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-23 15:09:00 +0000

    [Under_error_migrate23night]

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top', as: 'root'
   get 'home/about' => 'homes#about'
   #home/aboutのURLに飛んだ時、homesコントローラのindexアクションを実行
-  resources :books, only: [:new, :create, :index, :edit, :show, :destroy]
+  resources :books
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
