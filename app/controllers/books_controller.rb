dd03f95467cba82988c4b99a19ed0d055838c31b
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2023-11-25 12:49:12 +0000

    [Rast]bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+before_action :is_matching_login_user, only: [:edit, :update]
+
   def new
     @book = Book.new
     @books = Book.page(params[:page])
@@ -19,10 +21,10 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "You have updated book successfully." #updateアクションが成功したら遷移先画面でコメント表示
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
     else
     flash[:notice] = "Validation error: Please check the input."
     render 'edit'
@@ -59,6 +61,13 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id]) #ユーザID(URL)を持ってくる
+    unless book.user_id == current_user.id #ユーザID(URL)がログインIDと異なれば
+      redirect_to books_path #遷移させず指定のページへリダイレクトする
+    end
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,5 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
   #空欄はダメ、かつ、bodyは２００文字以内
-  validates :title, presence: true, uniqueness: true
-  #バリデーションを使用して、一意性を条件に追加。同じタイトルの本は登録できないようにした
 
 end
